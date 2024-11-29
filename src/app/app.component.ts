135d2d450d6bf6dd909a186669f64157443e9f80
WesleyEspinoza <wesleyespinoza1021@gmail.com>
2024-11-29 13:00:18 -0600

    populating the rest of the data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common'
-import bsodjsonData from "../../data/BSOD_DATA.json"
+import bsodjsonData from "../data/BSOD_DATA.json"
+
+
+interface bsodType {
+  stopCode: String
+  hexcode: string;
+  definition: string;
+  solution: string;
+}
+
+interface bsodArrayType {
+  bsodCodes: [bsodType]
+}
+
 
 @Component({
   selector: 'app-root',
@@ -11,13 +24,21 @@ import bsodjsonData from "../../data/BSOD_DATA.json"
 })
 
 export class AppComponent {
-  public keepOriginalOrder = (a: any, b: any) => a.key
-  bsodData: any
+  public keepOriginalOrder = (a: any, b: any) => a.key;
+  public bsodData: any = { bsodCodes: [] }
 
   ngOnInit() {
-
-    this.bsodData = bsodjsonData;
-
+    for (var key in bsodjsonData) {
+      if (bsodjsonData.hasOwnProperty(key)) {
+        const val = bsodjsonData[key as keyof typeof bsodjsonData];
+        const newCode: bsodType = { stopCode: "", hexcode: "", definition: "", solution: "", }
+        newCode.stopCode = key
+        newCode.hexcode = val["hexcode" as keyof typeof val]
+        newCode.definition = val["definition" as keyof typeof val]
+        newCode.solution = val["solution" as keyof typeof val]
+        this.bsodData.bsodCodes.push(newCode)
+      }
+    }
   }
   title = 'bsodLookup';
 }
